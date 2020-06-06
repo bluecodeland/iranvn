a177919c17f43064a3c1c25f30c0cc204f6d6c5d
bluecodeland <mhtaeb@gmail.com>
2020-06-06 20:48:12 +0430

    richat script added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,6 +53,7 @@
 
         gtag('config', 'UA-25404090-6');
         </script>
+        <script type="text/javascript">!function(){function t(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,localStorage.getItem("rayToken")?t.src="https://app.raychat.io/scripts/js/"+o+"?rid="+localStorage.getItem("rayToken")+"&href="+window.location.href:t.src="https://app.raychat.io/scripts/js/"+o;var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}var e=document,a=window,o="3aaea3cd-6b26-40a8-8a30-0f3f994f3d74";"complete"==e.readyState?t():a.attachEvent?a.attachEvent("onload",t):a.addEventListener("load",t,!1)}();</script>
 </head>
 <body>
     <div id="app">
